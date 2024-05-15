816e64cc97acc12090390f2fbec2d593982e21fd
TargetCoderX <mannasoumya009@gmail.com>
2024-05-16 00:55:16 +0530

    password encryption and verify

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,23 +1,34 @@
 const { connection } = require("../database/mysql");
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 const login = (req, res) => {
     let { email, password } = req.body;
     connection.connect();
-    connection.query(`select * from users where email='${email}' and password='${password}'`, (err, result, fields) => {
+    connection.query(`select * from users where email='${email}'`, (err, result, fields) => {
         if (!result.length)
             res.json({
-                "status": 1,
+                "status": 0,
                 "message": "No Record Found with this Email and Password",
             });
         else {
-            let token = jwt.sign({
-                'id': result[0].id,
-                'name': result[0].name,
-                'email': result[0].email,
-            }, process.env.JWT_SECRET);
-            res.json({
-                'status': 1,
-                'token': token,
+            bcrypt.compare(password.toString(), result[0].password, function (err, verify) {
+                if (verify) {
+                    let token = jwt.sign({
+                        'id': result[0].id,
+                        'name': result[0].name,
+                        'email': result[0].email,
+                    }, process.env.JWT_SECRET);
+                    res.json({
+                        'status': 1,
+                        'token': token,
+                    })
+                }else{
+                    res.json({
+                        "status": 0,
+                        "message": "No Record Found with this Email and Password",
+                    });
+                }
             })
         }
     });
--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { login } = require('../controllers/authController');
+const { getBecryptData } = require('../controllers/testController');
+
 const router1 = express.Router();
 const router2 = express.Router();
 
 /* guest routes */
 router1.post('/login', login)
+// router1.get('/hash',getBecryptData)
+
 
 /* protected route */
 
