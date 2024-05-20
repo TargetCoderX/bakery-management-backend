788b30e384c3ac0ad29a3645482c22d18bef2669
Soumya Manna <mannasoumya009@gmail.com>
2024-05-20 22:13:19 +0530

    differentiate array of customer and orders

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,12 +2,16 @@ const { connection } = require('../database/mysql');
 const getOrderOdCustomer = (req, res) => {
     let customerId = req.params.customer_id;
     let year = req.params.year;
-    connection.query(`select * from orders join customers on customers.id=orders.customer_id where orders.customer_id=${customerId} and orders.year=${year}`, (err, result, fields) => {
+    connection.query(`select * from orders where orders.customer_id=${customerId} and orders.year=${year}`, (err, result, fields) => {
         if (!err) {
-            res.json({
-                "status": 1,
-                "message": "",
-                "data": result,
+            let data = result;
+            connection.query(`select * from customers where id=${customerId}`, (err, result, fields) => {
+                res.json({
+                    "status": 1,
+                    "message": "",
+                    "data": data,
+                    "customer_data": result[0],
+                })
             })
         } else {
             res.json({
